04922c7f7cfb472665a3fe6050d431c7fed146a2
Patrick <patrick@threeshells.ovh>
2016-09-23 11:24:09 +0300

    virement system between compte universite and lycee done

--- src/Masca/TresorBundle/Controller/Lycee/CaisseLyceeController.php
+++ src/Masca/TresorBundle/Controller/Lycee/CaisseLyceeController.php
@@ -11,11 +11,15 @@ namespace Masca\TresorBundle\Controller\Lycee;
 
 use Doctrine\DBAL\Exception\ConstraintViolationException;
 use Masca\TresorBundle\Entity\MvmtLycee;
+use Masca\TresorBundle\Entity\MvmtUniversite;
 use Masca\TresorBundle\Entity\SoldeLycee;
+use Masca\TresorBundle\Entity\SoldeUniversite;
 use Masca\TresorBundle\Type\MvmtLyceeType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -83,7 +87,7 @@ class CaisseLyceeController extends Controller
         if($request->getMethod() == 'POST') {
             $form->handleRequest($request);
             if($mvmt->getSomme() <= 0) {
-                return $this->render('MascaTresorBundle:Universite:formulaire_operation.html.twig',[
+                return $this->render('MascaTresorBundle:Lycee:formulaire_operation.html.twig',[
                     'form'=>$form->createView(),
                     'error_message' => 'La somme entrée "'.$mvmt->getSomme().'" n\'est pas valide! '
                 ]);
@@ -108,6 +112,94 @@ class CaisseLyceeController extends Controller
                 }
                 $mvmt->setSoldeApres($solde->getSolde());
                 $em->persist($mvmt);
+                $em->flush();
+
+            } catch (ConstraintViolationException $e) {
+                return $this->render('MascaTresorBundle:Lycee:formulaire_operation.html.twig',[
+                    'form'=>$form->createView(),
+                    'error_message' => 'Une erreur d\'enregistrement vient de se produire, veuillez reesseyez svp! '
+                ]);
+            }
+            return $this->redirect($this->generateUrl('home_caisse_lycee'));
+        }
+        return $this->render('MascaTresorBundle:Lycee:formulaire_operation.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/virement", name="virement_to_universite")
+     */
+    public function virementToUniversiteAction(Request $request) {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_DAF')) {
+            return $this->render("::message-layout.html.twig", [
+                'message' => 'Vous n\'avez pas le droit d\'accès necessaire!',
+                'previousLink' => $request->headers->get('referer')
+            ]);
+        }
+        $mvmt = new MvmtLycee();
+        $mvmt->setTypeOperation('d');
+        $mvmt->setDescription('Virement compte Lycée à compte universite');
+
+        $form = $this->createForm(new MvmtLyceeType($this->getParameter('operation')),$mvmt);
+
+        $descriptionField = $form->get('description');
+        $options = $descriptionField->getConfig()->getOptions();
+        $options['disabled'] = true;
+        $form->add('description', TextType::class, $options);
+
+        $typeOperationField = $form->get('typeOperation');
+        $options = $typeOperationField->getConfig()->getOptions();
+        $options['disabled'] = true;
+        $form->add('typeOperation', ChoiceType::class, $options);
+
+        if($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if($mvmt->getSomme() <= 0) {
+                return $this->render('MascaTresorBundle:Lycee:formulaire_operation.html.twig',[
+                    'form'=>$form->createView(),
+                    'error_message' => 'La somme entrée "'.$mvmt->getSomme().'" n\'est pas valide! '
+                ]);
+            }
+            $em = $this->getDoctrine()->getManager();
+            try{
+                /**
+                 * @var $solde SoldeLycee
+                 */
+                $solde = $this->getDoctrine()->getRepository('MascaTresorBundle:SoldeLycee')->find(1);
+                $solde->setDate($mvmt->getDate());
+
+                $mvmt->setSoldePrecedent($solde->getSolde());
+
+                switch ($mvmt->getTypeOperation()) {
+                    case 'd':
+                        $solde->setSolde($solde->getSolde() - $mvmt->getSomme());
+                        break;
+                    case 'c':
+                        $solde->setSolde($solde->getSolde() + $mvmt->getSomme());
+                        break;
+                }
+                $mvmt->setSoldeApres($solde->getSolde());
+                $em->persist($mvmt);
+
+                $mvmtUniversite = new MvmtUniversite();
+                $mvmtUniversite->setDescription($mvmt->getDescription());
+                $mvmtUniversite->setTypeOperation('c');
+                $mvmtUniversite->setSomme($mvmt->getSomme());
+                $mvmtUniversite->setDate($mvmt->getDate());
+                /**
+                 * @var $soldeUniversite SoldeUniversite
+                 */
+                $soldeUniversite = $this->getDoctrine()->getRepository('MascaTresorBundle:SoldeUniversite')->find(1);
+                $soldeUniversite->setDate($mvmtUniversite->getDate());
+
+                $mvmtUniversite->setSoldePrecedent($soldeUniversite->getSolde());
+                $soldeUniversite->setSolde($soldeUniversite->getSolde() + $mvmtUniversite->getSomme());
+                $mvmtUniversite->setSoldeApres($soldeUniversite->getSolde());
+                $em->persist($mvmtUniversite);
+
                 $em->flush();
 
             } catch (ConstraintViolationException $e) {
--- src/Masca/TresorBundle/Controller/Universite/CaisseUniversiteController.php
+++ src/Masca/TresorBundle/Controller/Universite/CaisseUniversiteController.php
@@ -10,12 +10,17 @@ namespace Masca\TresorBundle\Controller\Universite;
 
 
 use Doctrine\DBAL\Exception\ConstraintViolationException;
+use Masca\TresorBundle\Entity\MvmtLycee;
 use Masca\TresorBundle\Entity\MvmtUniversite;
+use Masca\TresorBundle\Entity\SoldeLycee;
 use Masca\TresorBundle\Entity\SoldeUniversite;
+use Masca\TresorBundle\Type\MvmtLyceeType;
 use Masca\TresorBundle\Type\MvmtUniversiteType;
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
 use Symfony\Component\HttpFoundation\Request;
 
 /**
@@ -109,6 +114,94 @@ class CaisseUniversiteController extends Controller
                 }
                 $mvmt->setSoldeApres($solde->getSolde());
                 $em->persist($mvmt);
+                $em->flush();
+
+            } catch (ConstraintViolationException $e) {
+                return $this->render('MascaTresorBundle:Universite:formulaire_operation.html.twig',[
+                    'form'=>$form->createView(),
+                    'error_message' => 'Une erreur d\'enregistrement vient de se produire, veuillez reesseyez svp! '
+                ]);
+            }
+            return $this->redirect($this->generateUrl('home_caisse_universite'));
+        }
+        return $this->render('MascaTresorBundle:Universite:formulaire_operation.html.twig',[
+            'form'=>$form->createView()
+        ]);
+    }
+
+    /**
+     * @param Request $request
+     * @return \Symfony\Component\HttpFoundation\RedirectResponse|\Symfony\Component\HttpFoundation\Response
+     * @Route("/virement", name="virement_to_lycee")
+     */
+    public function virementToLyceeAction(Request $request) {
+        if (!$this->get('security.authorization_checker')->isGranted('ROLE_DAF')) {
+            return $this->render("::message-layout.html.twig", [
+                'message' => 'Vous n\'avez pas le droit d\'accès necessaire!',
+                'previousLink' => $request->headers->get('referer')
+            ]);
+        }
+        $mvmt = new MvmtUniversite();
+        $mvmt->setTypeOperation('d');
+        $mvmt->setDescription('Virement compte universitaire à compte Lycée');
+
+        $form = $this->createForm(new MvmtUniversiteType($this->getParameter('operation')),$mvmt);
+
+        $descriptionField = $form->get('description');
+        $options = $descriptionField->getConfig()->getOptions();
+        $options['disabled'] = true;
+        $form->add('description', TextType::class, $options);
+
+        $typeOperationField = $form->get('typeOperation');
+        $options = $typeOperationField->getConfig()->getOptions();
+        $options['disabled'] = true;
+        $form->add('typeOperation', ChoiceType::class, $options);
+
+        if($request->getMethod() == 'POST') {
+            $form->handleRequest($request);
+            if($mvmt->getSomme() <= 0) {
+                return $this->render('MascaTresorBundle:Universite:formulaire_operation.html.twig',[
+                    'form'=>$form->createView(),
+                    'error_message' => 'La somme entrée "'.$mvmt->getSomme().'" n\'est pas valide! '
+                ]);
+            }
+            $em = $this->getDoctrine()->getManager();
+            try{
+                /**
+                 * @var $solde SoldeUniversite
+                 */
+                $solde = $this->getDoctrine()->getRepository('MascaTresorBundle:SoldeUniversite')->find(1);
+                $solde->setDate($mvmt->getDate());
+
+                $mvmt->setSoldePrecedent($solde->getSolde());
+
+                switch ($mvmt->getTypeOperation()) {
+                    case 'd':
+                        $solde->setSolde($solde->getSolde() - $mvmt->getSomme());
+                        break;
+                    case 'c':
+                        $solde->setSolde($solde->getSolde() + $mvmt->getSomme());
+                        break;
+                }
+                $mvmt->setSoldeApres($solde->getSolde());
+                $em->persist($mvmt);
+
+                $mvmtLycee = new MvmtLycee();
+                $mvmtLycee->setDescription($mvmt->getDescription());
+                $mvmtLycee->setTypeOperation('c');
+                $mvmtLycee->setSomme($mvmt->getSomme());
+                $mvmtLycee->setDate($mvmt->getDate());
+                /**
+                 * @var $soldeLycee SoldeLycee
+                 */
+                $soldeLycee = $this->getDoctrine()->getRepository('MascaTresorBundle:SoldeLycee')->find(1);
+                $soldeLycee->setDate($mvmtLycee->getDate());
+
+                $mvmtLycee->setSoldePrecedent($soldeLycee->getSolde());
+                $soldeLycee->setSolde($soldeLycee->getSolde() + $mvmtLycee->getSomme());
+                $mvmtLycee->setSoldeApres($soldeLycee->getSolde());
+                $em->persist($mvmtLycee);
+
                 $em->flush();
 
             } catch (ConstraintViolationException $e) {
