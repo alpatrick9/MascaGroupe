9fd8f642134aa4d4f4eeaa16acdc03052e9899fd
Alain Patrick Rajaonarison <alain.patrick.raj@gmail.com>
2017-11-21 17:39:17 +0300

    gestion info statu

--- src/Masca/EtudiantBundle/Controller/Lycee/StatusEcoController.php
+++ src/Masca/EtudiantBundle/Controller/Lycee/StatusEcoController.php
@@ -15,6 +15,7 @@ use Sensio\Bundle\FrameworkExtraBundle\Configuration\ParamConverter;
 use Sensio\Bundle\FrameworkExtraBundle\Configuration\Route;
 use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\Extension\Core\Type\DateType;
+use Symfony\Component\HttpFoundation\Request;
 
 /**
  * Class StatusEcoController
@@ -28,16 +29,31 @@ class StatusEcoController extends Controller
      * @return \Symfony\Component\HttpFoundation\Response
      * @Route("/", name="home_state_eco_lycee")
      */
-    public function index() {
+    public function index(Request $request) {
+        $session = $this->get('session');
         $years = [];
         foreach(range(date('Y')-2,date('Y')) as $myyear) {
             $years[$myyear] = $myyear;
         }
-        $detailsSchoolYear = new DetailsSchoolYear();
+
+        if(empty($session->get('data')))
+            $detailsSchoolYear = new DetailsSchoolYear();
+        else {
+            $detailsSchoolYear = unserialize($session->get('data'));
+            $session->remove("data");
+        }
+
         $form = $this->createForm(DetailsSchoolYearType::class, $detailsSchoolYear, [
             'years' => $years,
             'months' => $this->getParameter('mois')
         ]);
+
+        if ($request->isMethod('POST')) {
+            $form->handleRequest($request);
+
+            $session->set('data', serialize($detailsSchoolYear));
+            return $this->redirect($this->generateUrl('home_state_eco_lycee'));
+        }
 
         return $this->render('MascaEtudiantBundle:Lycee:status_global_eco.html.twig', [
             'form' => $form->createView()
